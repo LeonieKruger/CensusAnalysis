35aa9b99a60d7fb9e6751332bf28f0280e598d61
LeonieKruger1 <leonie.vdbank@gmail.com>
2018-06-10 01:36:01 +0200

    Added SQL Script to count crimes

--- CrimeStats/CrimeWords.py
+++ CrimeStats/CrimeWords.py
@@ -3,21 +3,21 @@ from wordcloud import WordCloud
 import matplotlib.pyplot as plt
 import random
 
-def grey_color_func(word, font_size, position, orientation, random_state=None,
-                    **kwargs):
-    return "hsl(0, 0%%, %d%%)" % random.randint(10, 50)
+def random_color_func(word=None, font_size=None, position=None,  orientation=None, font_path=None, random_state=None):
+    h = int(100.0 * float(random.randint(10, 100)) / 255.0)
+    s = int(100.0 * 200.0 / 255.0)
+    l = int(100.0 * 128.0 / 255.0)
+    return "hsl({}, {}%, {}%)".format(h, s, l)
 
-
+cmap=plt.get_cmap('ocean')
 reader = csv.reader(open('/Users/leoniekruger/CencusData/Data/CrimeStats.csv', 'r',newline='\n'))
 d = {}
 for k,v in reader:
     d[k] = int(v)
 
-wc = WordCloud(width=600,height=150,background_color="white", max_words=20, max_font_size=200, relative_scaling=0.5, prefer_horizontal=0.4 , random_state=42).generate_from_frequencies(d)
+wc = WordCloud(width=400,height=150,background_color="white", max_words=20, max_font_size=200, relative_scaling=0.3, prefer_horizontal=0.4 , random_state=42).generate_from_frequencies(d)
 
-wc.recolor(color_func=grey_color_func, random_state=3)
-plt.imshow(wc.recolor(color_func=grey_color_func, random_state=3))
-
+plt.imshow(wc.recolor(color_func=random_color_func, random_state=30))
 plt.imshow(wc, interpolation='bilinear')
 plt.tight_layout(pad=0)
 plt.axis("off")
