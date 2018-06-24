0246418fb6d43945040bdd39b7b1ef781108b8eb
LeonieKruger1 <leonie.vdbank@gmail.com>
2018-06-24 09:57:29 +0200

    Added lables for population growth plots. Created poly1d fit

--- PopulationGrowth/PopulationGrowthPoly.py
+++ PopulationGrowth/PopulationGrowthPoly.py
@@ -1,5 +1,5 @@
-# @author Leonie Kruger
-# @project ELEN 7046
+import plotly.plotly as py
+import plotly.graph_objs as go
 
 # MatPlotlib
 import matplotlib.pyplot as plt
@@ -7,10 +7,6 @@ from matplotlib import pylab
 
 # Scientific libraries
 import numpy as np
-
-
-from scipy.optimize import curve_fit
-import plotly
 
 import csv
 
@@ -22,21 +18,17 @@ with open('/Users/leoniekruger/CencusData/Data/population_growth_sa.csv','r') as
     for row in plots:
         x.append(float(row[0]))
         y.append(float(row[1]))
+print(x)
+print(y)
+coef = np.polyfit(x, y, 1)
+p_func = np.poly1d(coef)
+# p30 = np.poly1d(np.polyfit(x, y, 30))
+xp = np.linspace(x[0], x[len(x)-1]+20, 100)
 
-def exponenial_func(x, a, b):
-    return a*np.exp(b*x)
-
-
-popt, pcov = curve_fit(exponenial_func, x, y,p0=(24,0.005),maxfev=1000)
-
-xx = np.linspace(x[0], x[len(x)-1]+20, 100)
-yy = exponenial_func(xx, *popt)
-
-plt.plot(x,y,'o', xx, yy)
-pylab.title('Exponential Fit')
 ax = plt.gca()
+ax.set_facecolor((0.898, 0.898, 0.898))
+pylab.title('Population Growth Linear Fit')
 ax.set_xlabel("Year", fontdict=None, labelpad=None)
 ax.set_ylabel("Population in Millions", fontdict=None, labelpad=None)
-ax.set_facecolor((0.898, 0.898, 0.898))
-fig = plt.gcf()
+plt.plot(x, y, '.', xp, p_func(xp))
 plt.show()
